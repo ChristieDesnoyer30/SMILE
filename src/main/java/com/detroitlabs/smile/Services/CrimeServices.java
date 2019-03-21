b27e4c0a8da7affc7bd2f52747f9d9e5eaddbfe9
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-03-21 15:29:36 -0400

    adds some error handling on index page if user doesnt enter a zip or Detroit for the city

--- src/main/java/com/detroitlabs/smile/Services/CrimeServices.java
+++ src/main/java/com/detroitlabs/smile/Services/CrimeServices.java
@@ -10,7 +10,7 @@ public class CrimeServices {
         RestTemplate rt = new RestTemplate();
         return rt.getForObject("https://data.detroitmi.gov/resource/6gdg-y3kf.json?$where=arrest_charge in('09001'," +
                         "'09002','09003',' 09005','12000','13002','36004','49000','52002','52003','54001')AND year in" +
-                        "('2018','2019')&block_id=" + blockID
+                        "('2017', '2018','2019')&block_id=" + blockID
                 ,TopCrimeData.class);
 
     }
@@ -19,7 +19,7 @@ public class CrimeServices {
         RestTemplate rt = new RestTemplate();
         return rt.getForObject("https://data.detroitmi.gov/resource/6gdg-y3kf.json?$where=arrest_charge in('10001'," +
                         " '13001','13003','23001','23002','23005','23006','24001','24002','35002','36003','37000','40002'," +
-                        "'40003','52001','53001','53002','63000','73000')AND year in('2018','2019')&block_id=" + blockID
+                        "'40003','52001','53001','53002','63000','73000')AND year in('2017', '2018','2019')&block_id=" + blockID
                 ,TopCrimeData.class);
 
     }
