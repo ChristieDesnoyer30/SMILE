10ab3f0c8beb504d837600ca5870a0da913c9ed0
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-04-02 13:40:49 -0400

    Fixed Lyft Services Test so that test can see environmental variables

--- src/main/java/com/detroitlabs/smile/Services/LyftServices.java
+++ src/main/java/com/detroitlabs/smile/Services/LyftServices.java
@@ -1,15 +1,22 @@
 package com.detroitlabs.smile.Services;
 import com.detroitlabs.smile.Model.LyftData.AllLyftData;
+import com.detroitlabs.smile.Model.LyftData.LocationInfo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
 
 @Component
 public class LyftServices {
 
     @Value("${LYFT_KEY}")
     private String lyftKey;
+
+    public void setLyftKey(String lyftKey) {
+        this.lyftKey = lyftKey;
+    }
 
     public AllLyftData fetchLyftData(double lat, double lng){
 
@@ -20,10 +27,27 @@ public class LyftServices {
         headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
         HttpEntity<String> httpEntity = new HttpEntity<>(headers);
 
-        ResponseEntity<AllLyftData> response = restTemplate.exchange("https://api.lyft.com/v1/nearby-drivers-pickup-etas?lat="+lat + "&lng=" + lng
+        ResponseEntity<AllLyftData> response = restTemplate.exchange("https://api.lyft.com/v1/nearby-drivers-pickup-etas?lat="+ lat + "&lng=" + lng
                 , HttpMethod.GET, httpEntity, AllLyftData.class);
 
         return response.getBody();
 
+    }
+
+
+    public Double[][] lyftCoordinateInfoForMaps(ArrayList<LocationInfo> allLocationInfo){
+
+        Double[][] lyftCoordsForMap = new Double[allLocationInfo.size()][];
+
+        for (int i =0; i< allLocationInfo.size(); i++) {
+            Double[] latAndLong = new Double[2];
+
+            latAndLong[0] = allLocationInfo.get(0).getLocations().get(0).getLat();
+            latAndLong[1] = allLocationInfo.get(0).getLocations().get(0).getLat();
+
+            lyftCoordsForMap[i] = latAndLong;
+        }
+
+        return lyftCoordsForMap;
     }
 }
